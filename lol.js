ace188940ff788196e094267fb8aa1f89ffe26f2
provide <102166958+provide-exe@users.noreply.github.com>
2022-06-25 19:23:03 +0100

    Update lol.js

--- lol.js
+++ lol.js
@@ -1,1 +1,1 @@
-console.log("Nice one")
+/* LMAOOO - You got scared for a second. You have been blacklisted from further using or buying my products. Please next time don't change the Credits. The license key that was used has been de-activated.*/
